a95826d444aa7580d42cf62958eecb79ed1df9cb
muzudho <muzudho1@gmail.com>
2026-01-11 13:31:40 +0900

    カレントディレクトリー出力修正（＾～＾）

--- Kifuwarapery/source/n080_common__/n080_200_utilProgram.cpp
+++ Kifuwarapery/source/n080_common__/n080_200_utilProgram.cpp
@@ -39,5 +39,20 @@ void UtilProgram::ShowCurrentDirectory()
 	TCHAR crDir[MAX_PATH + 1];
 	GetCurrentDirectory(MAX_PATH + 1, crDir);
 	crDir[MAX_PATH] = '\0';		// 念のため終端NULLを保証
-	SYNCCOUT << "Current directory=[" << crDir << "]" << SYNCENDL;
+
+	// crDir 配列をそのまま出力すると数になってしまった。
+	// Current directory=[000000E05BF83430]
+	//SYNCCOUT << "Current directory=[" << crDir << "]" << SYNCENDL;
+
+	// TCHARがwchar_t（Unicode）の場合、標準のcout系ストリームではアドレスが表示される問題を回避
+	// ここでは、WideCharToMultiByteでANSI文字列に変換して出力（簡易版。必要に応じてUTF-8などに調整）
+	char ansiDir[MAX_PATH + 1];
+	int ansiLen = WideCharToMultiByte(CP_ACP, 0, crDir, -1, ansiDir, MAX_PATH + 1, NULL, NULL);
+	if (ansiLen > 0) {
+		ansiDir[ansiLen - 1] = '\0'; // 安全のため
+		SYNCCOUT << "Current directory=[" << ansiDir << "]" << SYNCENDL;
+	}
+	else {
+		SYNCCOUT << "Current directory=[Error: Failed to convert path]" << SYNCENDL;
+	}
 }
