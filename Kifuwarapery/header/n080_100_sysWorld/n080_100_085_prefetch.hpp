df1cf4938017dc30fc7a69f2f04356b0458c90fe
muzudho <muzudho1@gmail.com>
2026-01-15 20:41:23 +0900

    コード整形

--- Kifuwarapery/header/n080_100_sysWorld/n080_100_085_prefetch.hpp
+++ Kifuwarapery/header/n080_100_sysWorld/n080_100_085_prefetch.hpp
@@ -20,28 +20,33 @@ template <typename T>
 inline void prefetch(T* addr)
 {
 #if defined HAVE_SSE2 || defined HAVE_SSE4
-#if defined(__INTEL_COMPILER)
-	// これでプリフェッチが最適化で消えるのを防げるらしい。
-	__asm__("");
-#endif
+
+
+	#if defined(__INTEL_COMPILER)
+		// これでプリフェッチが最適化で消えるのを防げるらしい。
+		__asm__("");
+	#endif
+
 
 	// 最低でも sizeof(T) のバイト数分をプリフェッチする。
 	// Stockfish は TTCluster が 64byte なのに、なぜか 128byte 分 prefetch しているが、
 	// 必要無いと思う。
 	char* charAddr = reinterpret_cast<char*>(addr);
-#if defined(__INTEL_COMPILER) || defined(_MSC_VER)
-	Unroller<(sizeof(T) + CacheLineSize - 1) / CacheLineSize>()([&](const int) {
-		// 1キャッシュライン分(64byte)のプリフェッチ。
-		_mm_prefetch(charAddr, _MM_HINT_T0);
-		charAddr += CacheLineSize;
-		});
-#else
-	Unroller<(sizeof(T) + CacheLineSize - 1) / CacheLineSize>()([&](const int) {
-		// 1キャッシュライン分(64byte)のプリフェッチ。
-		__builtin_prefetch(charAddr);
-		charAddr += CacheLineSize;
-		});
-#endif
+	#if defined(__INTEL_COMPILER) || defined(_MSC_VER)
+		Unroller<(sizeof(T) + CacheLineSize - 1) / CacheLineSize>()([&](const int) {
+			// 1キャッシュライン分(64byte)のプリフェッチ。
+			_mm_prefetch(charAddr, _MM_HINT_T0);
+			charAddr += CacheLineSize;
+			});
+	#else
+		Unroller<(sizeof(T) + CacheLineSize - 1) / CacheLineSize>()([&](const int) {
+			// 1キャッシュライン分(64byte)のプリフェッチ。
+			__builtin_prefetch(charAddr);
+			charAddr += CacheLineSize;
+			});
+	#endif
+
+
 #else
 	// SSE が使えない時は、_mm_prefetch() とかが使えないので、prefetch無しにする。
 	addr = addr; // warning 対策
--- Kifuwarapery/header/n080_100_sysWorld/n080_100_089_endian.hpp
+++ Kifuwarapery/header/n080_100_sysWorld/n080_100_089_endian.hpp
@@ -8,22 +8,22 @@
 
 
 #if 0
-#include <boost/detail/endian.hpp>
+	#include <boost/detail/endian.hpp>
 
 
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="T"></typeparam>
-/// <param name="r"></param>
-template <typename T>
-inline void reverseEndian(T& r)
-{
-	u8* begin = reinterpret_cast<u8*>(&r);
-	u8* IsEnd = reinterpret_cast<u8*>(&r) + sizeof(T);
-	for (; begin < IsEnd; ++begin, --IsEnd)
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="r"></param>
+	template <typename T>
+	inline void reverseEndian(T& r)
 	{
-		std::swap(*begin, *(IsEnd - 1));
+		u8* begin = reinterpret_cast<u8*>(&r);
+		u8* IsEnd = reinterpret_cast<u8*>(&r) + sizeof(T);
+		for (; begin < IsEnd; ++begin, --IsEnd)
+		{
+			std::swap(*begin, *(IsEnd - 1));
+		}
 	}
-}
 #endif
