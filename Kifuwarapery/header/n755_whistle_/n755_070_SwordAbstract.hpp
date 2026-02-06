f1c35c402ceeafe2c990a11fdefa9b2c17a38ad6
muzudho <muzudho1@gmail.com>
2026-02-06 23:48:13 +0900

    n755_whistle_

---
+++ Kifuwarapery/header/n755_whistle_/n755_070_SwordAbstract.hpp
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+
+#include "../n220_position/n220_650_position.hpp"
+#include "../n223_move____/n223_500_flashlight.hpp"
+#include "../n640_searcher/n640_440_splitedNode.hpp" // OurCarriage と持ち合い
+
+
+class OurCarriage;
+
+
+/// <summary>
+/// スプリット・ポイントは、剣を持っているんだぜ☆（＾ｑ＾）
+/// </summary>
+class SwordAbstract {
+
+
+public:
+
+
+	/// <summary>
+	///		<pre>
+	/// 依存関係の都合上、インラインにはしないぜ☆（＾ｑ＾）
+	///		</pre>
+	/// </summary>
+	/// <param name="ownerMonkeySplitedPlace"></param>
+	/// <param name="ourCarriage">わたしたちの馬車</param>
+	/// <param name="pos"></param>
+	/// <param name="pFlashlight"></param>
+	virtual void startSearch_asMonkeySplitedPlace(
+		MonkeySplitedPlace& ownerMonkeySplitedPlace,
+		OurCarriage& ourCarriage,
+		Position& pos,
+		Flashlight* pFlashlight
+		) const = 0;
+};
---
+++ Kifuwarapery/source/n755_whistle_/n755_120_SwordNonPv.cpp
@@ -0,0 +1,36 @@
+﻿#include "../../header/n755_whistle_/n755_120_SwordNonPv.hpp"
+#include "../../header/n887_nodeKind/n887_150_adventurePlainNodekindSplitedNodeNonPv.hpp"
+
+
+const SwordNonPv g_SWORD_NON_PV;
+
+
+/// <summary>
+///		<pre>
+///		- スプリット・ポイントでの探索開始？
+///		- 依存関係の都合上、インラインにはしないぜ☆（＾ｑ＾）
+///		</pre>/// </summary>
+/// <param name="ownerMonkeySplitedPlace"></param>
+/// <param name="ourCarriage"></param>
+/// <param name="pos"></param>
+/// <param name="pFlashlight"></param>
+void SwordNonPv::startSearch_asMonkeySplitedPlace(
+	MonkeySplitedPlace& ownerMonkeySplitedPlace, 
+	OurCarriage& ourCarriage,
+	Position& pos,
+	Flashlight* pFlashlight) const
+{
+	//────────────────────────────────────────────────────────────────────────────────
+	// 探索☆？（＾ｑ＾）
+	//────────────────────────────────────────────────────────────────────────────────
+	// スプリットポイントは検索を開始するぜ☆（＾ｑ＾）
+	g_NODEKIND_MONKEYSPLITEDPLACE_NON_PV.explorePlain_10i(
+		ourCarriage,
+		pos,
+		pFlashlight + 1,
+		ownerMonkeySplitedPlace.m_alpha,
+		ownerMonkeySplitedPlace.m_beta,
+		ownerMonkeySplitedPlace.m_depth,
+		ownerMonkeySplitedPlace.m_cutNode
+		);
+}
