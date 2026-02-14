2f3d224e83dbd5591e73ea168fdd983f06bdebe2
muzudho <muzudho1@gmail.com>
2026-02-14 14:51:45 +0900

    EngineOption 調べ（＾～＾）

--- Kifuwarapery/src/layer_game_engine/setup_by_engine_options.cpp
+++ Kifuwarapery/src/layer_game_engine/setup_by_engine_options.cpp
@@ -62,21 +62,52 @@ namespace {
 /// <param name="pRucksack"></param>
 void EngineOptionSetup::initialize_10a500b100c(EngineOptionCollection* pMap, GameEngineStorageOurCarriage * pRucksack)
 {
+	// ハッシュサイズ
 	pMap->Put("USI_Hash"					, EngineOption(256, 1, 65536, onHashSize, pRucksack));
+
+	// ［ハッシュ・クリアー］ボタン
 	pMap->Put("Clear_Hash"					, EngineOption(onClearHash, pRucksack));
+
+	// 定跡ファイルパス
 	pMap->Put("Book_File"					, EngineOption("book/20150503/book.bin"));
+
+	// ［定跡］チェックボックス
 	pMap->Put("Best_Book_Move"				, EngineOption(false));
+
+	// ［定跡］チェックボックス
 	pMap->Put("OwnBook"						, EngineOption(true));
+
+	// 定跡の最小手数
 	pMap->Put("Min_Book_Ply"				, EngineOption(SHRT_MAX, 0, SHRT_MAX));
+
+	// 定跡の最大手数
 	pMap->Put("Max_Book_Ply"				, EngineOption(SHRT_MAX, 0, SHRT_MAX));
+
+	// 定跡評価値
 	pMap->Put("Min_Book_Score"				, EngineOption(-180, -SweetnessInfinite, SweetnessInfinite));
+
+	// ［評価値ファイル］フォルダー
 	pMap->Put("Eval_Dir"					, EngineOption("20151105", onEvalDir));
+
+	// ［評価値書き込み］チェックボックス
 	pMap->Put("Write_Synthesized_Eval"		, EngineOption(false));
+
+	// ［相手の手番に考えるか］チェックボックス
 	pMap->Put("USI_Ponder"					, EngineOption(true));
-	pMap->Put("Byoyomi_Margin"				, EngineOption(500, 0, INT_MAX));//元の値：0.5秒☆？（＾ｑ＾）
+
+	// ［秒読みマージン］ - 元の値：0.5秒☆？（＾ｑ＾）
+	pMap->Put("Byoyomi_Margin"				, EngineOption(500, 0, INT_MAX));
+
+	// ［几帳面な読み］の本数
 	pMap->Put("MultiPV"						, EngineOption(1, 1, Move::m_MAX_LEGAL_MOVES));
+
+	// ［コンピューターの強さ］。遊び用
 	pMap->Put("Skill_Level"					, EngineOption(20, 0, 20));
+
+	// ［最大ランダム評価値差］
 	pMap->Put("Max_Random_Score_Diff"		, EngineOption(0, 0, SweetnessMate0Ply));
+
+	// ［最大ランダム評価値差の手数］
 	pMap->Put("Max_Random_Score_Diff_Ply"	, EngineOption(40, 0, SHRT_MAX));
 
 	//────────────────────────────────────────────────────────────────────────────────
@@ -95,9 +126,16 @@ void EngineOptionSetup::initialize_10a500b100c(EngineOptionCollection* pMap, Gam
 	//────────────────────────────────────────────────────────────────────────────────
 	//
 	//────────────────────────────────────────────────────────────────────────────────
+
+	// ［一緒に走る猿が分岐する点の最大数］
 	pMap->Put("Max_Threads_per_Split_Point"	, EngineOption(		5,  4,     8, onThreads, pRucksack));
+
+	// スレッド数
 	pMap->Put("Threads"						, EngineOption(cpuCoreCount(), 1, g_MaxThreads, onThreads, pRucksack));
+
+	// ［寝てる猿を使う］チェックボックス
 	pMap->Put("Use_Sleeping_Threads"		, EngineOption(false));
+
 #if defined BISHOP_IN_DANGER
 	(*this)["Danger_Demerit_Score"] = EngineOption(700, SHRT_MIN, SHRT_MAX);
 #endif
