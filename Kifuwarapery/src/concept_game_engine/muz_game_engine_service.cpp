868cb073e96bc36a5289858163eaa4582c9407ef
muzudho <muzudho1@gmail.com>
2026-02-27 20:34:31 +0900

    フォルダー構成変更

--- Kifuwarapery/src/concept_game_engine/muz_game_engine_service.cpp
+++ Kifuwarapery/src/concept_game_engine/muz_game_engine_service.cpp
@@ -1,10 +1,10 @@
 #include "../core_library/lib_95a_cpp/muz_string_service.hpp"
 #include "../core_library/lib_94a_cli/muz_cli_service.hpp"
 #include "../lib_5a_gui/muz_engine_settings_initialize_service.hpp"
-#include "../lib_5a_toybox_1b_hand_stand/color.hpp"
+#include "../core_library/lib_55a_toybox_95b_hand_stand/color.hpp"
 #include "../view_5a_toybox/muz_5a1b_hand_stand_view.hpp"
 #include "../view_5a_toybox/muz_5a25b_board_view.hpp"
-#include "../lib_5a_toybox_1b_hand_stand/muz_hand_stand_collection_service.hpp"
+#include "../core_library/lib_55a_toybox_95b_hand_stand/muz_hand_stand_collection_service.hpp"
 #include "muz_game_engine_service.hpp"
 
 using namespace std;
--- Kifuwarapery/src/lib_5a_toybox_n2/piece_types.hpp
+++ Kifuwarapery/src/lib_5a_toybox_n2/piece_types.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../lib_5a_toybox_n1/muz_position_n1_model.hpp"
-#include "../lib_5a_toybox_1b_hand_stand/muz_hand_stand_model.hpp"
+#include "../core_library/lib_55a_toybox_95b_hand_stand/muz_hand_stand_model.hpp"
 
 /// <summary>
 /// 
